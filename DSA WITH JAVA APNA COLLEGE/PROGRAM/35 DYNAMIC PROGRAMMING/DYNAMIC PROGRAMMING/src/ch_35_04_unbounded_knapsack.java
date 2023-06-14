29deb81120cbdea15352e7d71264cd4448f4876a
Pawankumarmeena425 <pawankuamrmeena425@gmail.com>
2023-06-14 16:58:33 +0530

    adding unbounded knapsack and coin changes

---
+++ DSA WITH JAVA APNA COLLEGE/PROGRAM/35 DYNAMIC PROGRAMMING/DYNAMIC PROGRAMMING/src/ch_35_04_unbounded_knapsack.java
@@ -0,0 +1,54 @@
+public class ch_35_04_unbounded_knapsack {
+
+    public static int unboundedKnapsack(int val[] , int wt[] , int W){
+        int n=val.length;
+        int dp[][] = new int[val.length+1][W+1];
+//        initlize all element of the array with -1
+        for(int i=0;i<dp.length;i++){
+            for(int j =0;j<dp[0].length;j++){
+                dp[i][j] = -1;
+            }
+        }
+
+//      fill with 0 , 0th column and 0th row
+        for(int i=0;i<dp.length;i++){
+            dp[i][0] = 0;
+        }
+        for(int j = 0;j<dp[0].length;j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[0].length;j++){
+                int v = val[i-1];
+                int w = wt[i-1]; // ith item weight and values
+
+                if(w<=j){ // include case
+                    dp[i][j] = Math.max(v+dp[i][j-w] , dp[i-1][j]);
+                }
+                else{ // exclude
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+    printTable(dp);
+       return dp[n][W];
+
+    }
+    public static void printTable(int dp[][]){
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                System.out.print(dp[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int val[] = {15,14,10,45,30};
+        int wt[] = {2,5,1,3,4};
+        int w = 7;
+        System.out.println(unboundedKnapsack(val,wt,w));
+    }
+}
